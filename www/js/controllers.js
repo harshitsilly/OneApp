ecc7797da5d52972fce28da467428880172709cb
Upendra Gupta <upendragupta.iiita@gmail.com>
2016-12-12 17:02:09 +0530

    meeting request screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -77,6 +77,11 @@ angular.module('app.controllers', [])
 })
 
 .controller('newMeetingRequestCtrl', function ($scope, $rootScope, $state) {
+
+   $scope.detailNav = function () {
+   
+    $state.go("page.detail");
+  };
 })
 .controller('meetingCtrl', function ($scope, $rootScope, $state) {
 
